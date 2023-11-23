d79d01dec34b2348a5f244e8dc1bf78f2c18f0fb
Sergiioog <sergiogargor9@gmail.com>
2023-11-23 21:25:05 +0100

    Inicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BodyComponent } from './componentes/body/body.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
