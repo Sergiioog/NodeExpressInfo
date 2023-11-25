58de97d196eb3a5ac3294ca615bc90b7ea2c032f
Sergiioog <sergiogargor9@gmail.com>
2023-11-25 22:12:25 +0100

    Nodejs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NodepagComponent } from './componentes/body/nodepag/nodepag.component';
+import { ExpresspagComponent } from './componentes/body/expresspag/expresspag.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'node', component: NodepagComponent},
+  { path: 'express', component: ExpresspagComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
